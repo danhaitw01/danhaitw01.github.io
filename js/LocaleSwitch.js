0484054f72ff71a83ace0db2a48bd0b4db3246bd
rumamitw01 <42371341+rumamitw01@users.noreply.github.com>
2023-04-14 00:08:10 +0800

    Test separated code

--- js/LocaleSwitch.js
+++ js/LocaleSwitch.js
@@ -4,3 +4,29 @@ $(document).ready(function () {
     $(this).html($(this).data("lang-" + l));
   };
 });
+function English_Locale(){
+	$('#LocaleReq0').jqmultilang('en');
+	$('#LocaleReq1').jqmultilang('en');
+	$('#LocaleReq2').jqmultilang('en');
+	$('#LocaleReq3').jqmultilang('en');
+	$('#LocaleReq4').jqmultilang('en');
+	$('#LocaleReq5').jqmultilang('en');
+	$('#LocaleReq6').jqmultilang('en');}
+function Chinese_Locale(){
+	$('#LocaleReq0').jqmultilang('zh-tw');
+	$('#LocaleReq1').jqmultilang('zh-tw');
+	$('#LocaleReq2').jqmultilang('zh-tw');
+	$('#LocaleReq3').jqmultilang('zh-tw');
+	$('#LocaleReq4').jqmultilang('zh-tw');
+	$('#LocaleReq5').jqmultilang('zh-tw');
+	$('#LocaleReq6').jqmultilang('zh-tw');
+}
+function Japanese_Locale(){
+	$('#LocaleReq0').jqmultilang('ja');
+	$('#LocaleReq1').jqmultilang('ja');
+	$('#LocaleReq2').jqmultilang('ja');
+	$('#LocaleReq3').jqmultilang('ja');
+	$('#LocaleReq4').jqmultilang('ja');
+	$('#LocaleReq5').jqmultilang('ja');
+	$('#LocaleReq6').jqmultilang('ja');
+}
