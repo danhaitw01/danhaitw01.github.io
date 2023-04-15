39960884dd97d2da9f5c096a62b2a119badcd13c
danhaitw01 <42371341+rumamitw01@users.noreply.github.com>
2023-04-15 19:38:22 +0800

    Locale Switch Feature Ver2 Updated

--- js/LocaleSwitch.js
+++ js/LocaleSwitch.js
@@ -1,6 +1,64 @@
-document.write('<script src="./js/jquery-3.6.1.js"></script>');
-$(document).ready(function () {
-  $.fn.jqmultilang = function (l) {
-    $(this).html($(this).data("lang-" + l));
-  };
-});
+//這裡註解掉是因為在main.js中已引入jquery，不用引入第二次，一般測試情況請引入jquery
+//document.write('<script src="./js/jquery-3.6.1.js"></script>');
+
+//第一版寫法
+$(document).ready(
+	function () {
+		$.fn.jqmultilang = 
+		function (l) {
+			$(this).html($(this).data("lang-" + l));
+		};
+	}
+);
+
+function Locale_Switch_V1(times,input){
+	$('#LocaleReq'+String(times)).jqmultilang(input);
+}
+
+
+
+//第二版寫法
+
+var Chinese_String=[
+"語言切換",
+"琉見的個人頁面",
+"我的Twitter",
+"我的Facebook粉絲專頁",
+"我的Twitch頻道",
+"我Youtube上的翻譯頻道",
+"我Youtube上的娛樂頻道",
+"我的bilibili個人空間"];
+
+var English_String=[
+"Change Language",
+"Rumami's Profile Page",
+"My Twitter",
+"My Facebook Page",
+"My Twitch Channel",
+"My Translation Youtube Channel",
+"My Entertainment Youtube Channel",
+"My bilibili Space"];
+
+var Japanese_String=[
+"言語切り替え",
+"ルマミの紹介ページ",
+"私のTwitter",
+"私のFacebookページ",
+"私のTwitchチャンネル",
+"私の翻訳Youtubeチャンネル",
+"私のエンタメYoutubeチャンネル",
+"私のbilibili空間"];
+
+function Locale_Switch_V2(locale){
+	for(var i=0;i<Chinese_String.length+1;i++){
+		if(locale=="zh-tw"){
+			$('#LocaleReq'+String(i)).text(Chinese_String[i])
+		}
+		else if(locale=="en"){
+			$('#LocaleReq'+String(i)).text(English_String[i])
+		}
+		else if(locale=="ja"){
+			$('#LocaleReq'+String(i)).text(Japanese_String[i])
+		}
+	}
+}
--- js/main.js
+++ js/main.js
@@ -1,20 +1,43 @@
 document.write('<script src="./js/jquery-3.6.1.js"></script>');
 document.write('<script src="./js/LocaleSwitch.js"></script>');
-function English_Locale(){
+//第一版寫法
+
+function English_Locale_V1(){
 	document.title="Rumami's Profile Page";
-	for (var i=0;i<7;i++){
-		$('#LocaleReq'+String(i)).jqmultilang('en');
+	for (var i=0;i<8;i++){
+		Locale_Switch_V1(i,'en');
 	}
 }
-function Chinese_Locale(){
+
+function Chinese_Locale_V1(){
 	document.title="琉見的個人頁面";
-	for (var i=0;i<7;i++){
-		$('#LocaleReq'+String(i)).jqmultilang('zh-tw');
+	for (var i=0;i<8;i++){
+		Locale_Switch_V1(i,'zh-tw');
 	}
 }
-function Japanese_Locale(){
+
+function Japanese_Locale_V1(){
 	document.title="ルマミの紹介ページ";
-	for (var i=0;i<7;i++){
-		$('#LocaleReq'+String(i)).jqmultilang('ja');
+	for (var i=0;i<8;i++){
+		Locale_Switch_V1(i,'ja');
 	}
+}
+
+
+
+//第二版寫法
+
+function Chinese_Locale_V2(){
+	document.title="琉見的個人頁面";
+	Locale_Switch_V2("zh-tw")
+}
+
+function English_Locale_V2(){
+	document.title="Rumami's Profile Page";
+	Locale_Switch_V2("en")
+}
+
+function Japanese_Locale_V2(){
+	document.title="ルマミの紹介ページ";
+	Locale_Switch_V2("ja")
 }
